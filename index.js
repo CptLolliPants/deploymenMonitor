649cbfa1d5ac030f3d8bb4c7e553c43ee6d97bd5
Martin Caarels <martin@caarels.com>
2018-12-13 10:37:42 +0100

    basic express scaffold

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'assets')))
+app.get('/', (req, res) => {
+  res.sendFile(
+    path.join(__dirname, 'views/index.html')
+  )
+})
+app.post('/api', (req, res) => {
+  var body = req.body
+  console.log(req.body)
+  res.send(req.body.data)
+})
+
+app.listen(port, () => console.log('Example app listening on port: ' + port))
