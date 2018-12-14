6cfb3365d85ced704c51e8db827358a9222ffd25
Sjoerd de Jong <s.de.jong@issc.leidenuniv.nl>
2018-12-14 09:39:51 +0100

    Laatste poes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
+const secrets = require('./secrets.json')
+
+console.log(secrets)
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'assets')))
