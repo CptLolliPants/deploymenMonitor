c3ab6923e7e37ef73dd5161fb210ca51a0f2d9b4
Martin Caarels <martin@caarels.com>
2018-12-14 10:59:24 +0100

    incorporated callback

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
 let test = require('./lib/vroConnector.js')
 let credentials = require('./secrets.json')
 
-console.log(JSON.stringify(test.getMultiMachineDeployments(
+test.getMultiMachineDeployments(
   'https://p-issc-000015.luci.leidenuniv.nl:8281/vco/api/categories/2c912018667d522501671c3996135815',
-  credentials
-  )));
+  credentials,
+  function(result){
+    console.log(result)
+  }
+  )
