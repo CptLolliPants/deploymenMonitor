ac77cbe7a70740bc5c6c033c0a32c24e5061e03b
Martin Caarels <martin@caarels.com>
2018-12-14 10:52:58 +0100

    test:

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 let test = require('./lib/vroConnector.js')
 let credentials = require('./secrets.json')
 
-console.log(test.getMultiMachineDeployments(
+console.log(JSON.stringify(test.getMultiMachineDeployments(
   'https://p-issc-000015.luci.leidenuniv.nl:8281/vco/api/categories/2c912018667d522501671c3996135815',
   credentials
-  ));
+  )));
