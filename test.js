098d2c5ac4511746805a0bffe8f838fe8a6357ae
Martin Caarels <martin@caarels.com>
2018-12-14 10:46:26 +0100

    added credentials to test

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 let test = require('./lib/vroConnector.js')
-console.log(test.getMultiMachineDeployments('https://p-issc-000015.luci.leidenuniv.nl:8281/vco/api/categories/2c912018667d522501671c3996135815'));
+let credentials = require('./secrets.json')
+
+console.log(test.getMultiMachineDeployments(
+  'https://p-issc-000015.luci.leidenuniv.nl:8281/vco/api/categories/2c912018667d522501671c3996135815',
+  credentials
+  ));
