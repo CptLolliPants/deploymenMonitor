233ca1a091b29a3a68088603ab3d2ece1bd62eb0
Martin Caarels <martin@caarels.com>
2018-12-14 11:00:43 +0100

    lambda expressions ftw

--- test.js
+++ test.js
@@ -4,7 +4,5 @@ let credentials = require('./secrets.json')
 test.getMultiMachineDeployments(
   'https://p-issc-000015.luci.leidenuniv.nl:8281/vco/api/categories/2c912018667d522501671c3996135815',
   credentials,
-  function(result){
-    console.log(result)
-  }
-  )
+  (result) => console.log(result)
+)
