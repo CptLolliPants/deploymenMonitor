e15cb91e2a958ba87d06fbe0b0a8d467dad7d926
Martin Caarels <martin@caarels.com>
2018-12-14 15:29:28 +0100

    assets/js/main:

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,17 @@
-() => {
-    let xhttp = new XMLHttpRequest()
-    let myInput = document.getElementById("myInput")
-    let myOutput = document.getElementById("myOutput")
-    xhttp.open(
-        "GET",
-        "api"
-    )
-    xhttp.setRequestHeader(
-        "Content-type",
-        "application/json;charSet=UTF-8"
-    )
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == XMLHttpRequest.DONE && this.status === 200){
-            JSON.parse(this.response).forEach((el) => myOutput.innerHTML += '<br>' + el.id)
-        }
+let xhttp = new XMLHttpRequest()
+let myInput = document.getElementById("myInput")
+let myOutput = document.getElementById("myOutput")
+xhttp.open(
+    "GET",
+    "api"
+)
+xhttp.setRequestHeader(
+    "Content-type",
+    "application/json;charSet=UTF-8"
+)
+xhttp.onreadystatechange = function(){
+    if(this.readyState == XMLHttpRequest.DONE && this.status === 200){
+        JSON.parse(this.response).forEach((el) => myOutput.innerHTML += '<br>' + el.id)
     }
-    xhttp.send(JSON.stringify({"data":myInput.value}))
-}()
+}
+xhttp.send(JSON.stringify({"data":myInput.value}))
