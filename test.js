5aafdeef2ed3bb4b7da1ea177a7d06e9c69e268f
Martin Caarels <martin@caarels.com>
2018-12-14 10:27:47 +0100

    created module and test

---
+++ test.js
@@ -0,0 +1,2 @@
+let test = require('./lib/vroConnector.js')
+console.log(test.getMultiMachineDeployments('https://p-issc-000015.luci.leidenuniv.nl:8281/vco/api/categories/2c912018667d522501671c3996135815'));
