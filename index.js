7f92b69813f57ccfc72d8d5148863022b2bc255a
Sjoerd de Jong <s.de.jong@issc.leidenuniv.nl>
2018-12-21 15:33:12 +0100

    porduciton

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express()
 const port = process.env.PORT || 3000
 const credentials = require('./secrets.json')
 const vro = require('./lib/vroConnector.js')
-const url = 'https://p-issc-000015.luci.leidenuniv.nl:8281/vco/api/categories/2c912018667d522501671c3996135815'
+//const url = 'https://p-issc-000015.luci.leidenuniv.nl:8281/vco/api/categories/2c912018667d522501671c3996135815'
+const url = 'https://portal.luci.leidenuniv.nl:443/vco/api/categories/2c9120b365371a91016652a534072dcb/'
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'assets')))
